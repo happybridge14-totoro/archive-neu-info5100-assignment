1c4bf19488ffa9fe923deb09074679886dfdb208
Yiji Huang <happybridge14@gmail.com>
2019-10-25 07:10:16 -0700

    :construction: Finish q2

---
+++ assignment_06/q2/Test.java
@@ -0,0 +1,9 @@
+package q2;
+
+class Test {
+  public static void main(String[] args) {
+    System.out.println("Write two differences between ArrayList and LinkedList.");
+    System.out.println("1. ArrayList support random access and LinkedList has no random access capabilities");
+    System.out.println("2. Adding into ArrayList is of O(n) in worst case, while adding is O(1) operation in LinkedList");
+  }
+}
